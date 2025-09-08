43e009e97d1dd908fd74066288f3f5d0a135524e
DB030624 <dylan.bath24@icloud.com>
2025-09-08 10:31:32 -0700

    algos fixed

--- lab1.py
+++ lab1.py
@@ -1,24 +1,34 @@
-def linear_search(needle,haystack):
-    for i in haystack:
-        if i == needle:
-            return True
-        else:
-            return False
+def linear_search(needle, haystack):
+    for i in range(len(haystack)):
+        if haystack[i] == needle:
+            return i
+    return None
+
         
-def binary_search(needle,haystack):
+def binary_search(needle, haystack):
     l = 0
-    r = len(haystack) - 1
     haystack = sorted(haystack)
+    r = len(haystack) - 1
+
     while l <= r:
         mid = (l + r) // 2
-
         if haystack[mid] == needle:
-            return True
+            return mid
         elif haystack[mid] < needle:
             l = mid + 1
         else:
             r = mid - 1
-    return False
+    return None
+
+
+print(linear_search(8, [6, 2, 8, 4])) 
+print(linear_search(4, [6, 2, 8, 4])) 
+print(linear_search(5, [6, 2, 8, 4]))  
+
+print(binary_search(8, [2, 4, 6, 8]))  
+print(binary_search(2, [2, 4, 6, 8]))  
+print(binary_search(1, [2, 4, 6, 8]))  
+print(binary_search(99, [2, 4, 6, 8])) 
 
 
 
