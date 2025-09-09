2ded59ade3600477c6b8a00b7c4bcf11b6b5eb22
DB030624 <dylan.bath24@icloud.com>
2025-09-09 12:24:24 -0700

    code

--- lab1.py
+++ lab1.py
@@ -31,5 +31,42 @@ print(binary_search(1, [2, 4, 6, 8]))
 print(binary_search(99, [2, 4, 6, 8])) 
 
 
+def linear_search_multi(needle, haystack):
+    indexes = []
+    for i in range(len(haystack)):
+        if needle == haystack[i]:
+            indexes.append(i)
+    return indexes
 
-        
+
+def linear_seach_binary_multi(needle,haystack):
+    indexes = []
+    l = 0
+    r = len(haystack) - 1
+    while l <= r:
+        mid = (r + l) // 2
+        if haystack[mid] == needle:
+            indexes.append(mid)
+
+            i = mid - 1
+            while i >= 0 and haystack[i] == needle:
+                indexes.append(i)
+
+                i-= 1
+            
+            i = mid + 1
+            while i <= len(haystack) - 1 and haystack[i] == needle:
+                indexes.append(i)
+                i+= 1
+            return sorted(indexes)
+        elif haystack[mid] < needle:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+
+    return indexes
+
+
+
+
